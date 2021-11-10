4a1d004b56e32d20fb1ae625fa2bcd7e12a08379
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-11-10 14:19:51 +0000

    tags not supported

--- amp.tf
+++ amp.tf
@@ -2,9 +2,4 @@ resource "aws_prometheus_workspace" "amp" {
   provider = aws.ireland
   alias    = var.amp_alias
 
-  tags = {
-    Name        = var.amp_alias
-    Description = "Managed by Terraform"
-    Owner       = var.team_name
-  }
 }
