0e9ea34c8766789e4e39b6f498ff20a34bfaec79
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-11-10 19:52:16 +0000

    example

--- example/prometheus.tf
+++ example/prometheus.tf
@@ -1,6 +1,7 @@
 module "test_prometheus" {
-  source            = "../"
-  amp_alias         = "test-prometheus"
-  team_name         = "test-team"
-  oidc_provider_url = "https://oidc/etc"
+  source              = "../"
+  amp_alias           = "test-prometheus"
+  team_name           = "test-team"
+  oidc_provider_url   = "https://oidc/etc"
+  cluster_domain_name = "test.cloud-platform.service.justice.gov.uk"
 }
