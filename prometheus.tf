0a1bf249bc1ebf3a0793096ebd0047a48392f667
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-11-10 16:50:23 +0000

    ns & prom

---
+++ prometheus.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  namespace  = kubernetes_namespace.monitoring.id
+  version    = "14.11.1"
+}
