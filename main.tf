b432a6aed2bffb5b7a18a3ff5b142404386a607e
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-11-10 12:55:54 +0000

    regions

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+  alias   = "ireland"
+}
+
+provider "aws" {
+  region  = "eu-west-2"
+  alias   = "london"
+}
+
 resource "aws_prometheus_workspace" "amp" {
+  provider = aws.london
   alias = var.amp_alias
 }
