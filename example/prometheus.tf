4869abe8e14dcf141e04e5c3db9360835904ee27
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-11-10 18:48:20 +0000

    example

--- example/prometheus.tf
+++ example/prometheus.tf
@@ -1,5 +1,6 @@
 module "test_prometheus" {
-  source    = "../"
-  amp_alias = "test-prometheus"
-  team_name = "test-team"
+  source            = "../"
+  amp_alias         = "test-prometheus"
+  team_name         = "test-team"
+  oidc_provider_url = "https://oidc/etc"
 }
