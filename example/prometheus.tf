914938392ad03c2fec55d178d4ab5b5b106e9a43
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-11-11 21:46:55 +0000

    example

--- example/prometheus.tf
+++ example/prometheus.tf
@@ -4,4 +4,11 @@ module "test_prometheus" {
   team_name           = "test-team"
   oidc_provider_url   = "https://oidc/etc"
   cluster_domain_name = "test.cloud-platform.service.justice.gov.uk"
+  alertmanager_slack_receivers = [
+    {
+      severity = "warning"
+      webhook  = "https://hooks.slack.com/services/dummy"
+      channel  = "#lower-priority-alarms"
+    }
+  ]
 }
