fa487d9ce2fd2b7fb351f5e0be02d59e9e2036ca
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-11-15 09:38:30 +0000

    prom proxy

--- prometheus.tf
+++ prometheus.tf
@@ -12,3 +12,44 @@ resource "helm_release" "prometheus" {
   depends_on = [kubernetes_service_account.irsa_prometheus_sa]
 
 }
+
+# Prometheus proxy
+# Ref: https://github.com/evry/docker-oidc-proxy
+resource "random_id" "session_secret" {
+  byte_length = 16
+}
+
+data "template_file" "prometheus_proxy" {
+  template = file("${path.module}/templates/oauth2-proxy.yaml.tpl")
+
+  vars = {
+    upstream      = "http://prometheus-server:80"
+    hostname      = local.prometheus_ingress
+    exclude_paths = "^/-/healthy$"
+    issuer_url    = var.oidc_issuer_url
+    client_id     = var.oidc_components_client_id
+    client_secret = var.oidc_components_client_secret
+    cookie_secret = random_id.session_secret.b64_std
+    clusterName   = var.amp_alias
+  }
+}
+
+resource "helm_release" "prometheus_proxy" {
+  name       = "prometheus-proxy"
+  namespace  = kubernetes_namespace.monitoring.id
+  repository = "https://charts.helm.sh/stable"
+  chart      = "oauth2-proxy"
+  version    = "3.2.2"
+
+  values = [
+    data.template_file.prometheus_proxy.rendered,
+  ]
+
+  depends_on = [
+    random_id.session_secret,
+  ]
+
+  lifecycle {
+    ignore_changes = [keyring]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -31,3 +31,15 @@ variable "alertmanager_slack_receivers" {
   description = "A list of configuration values for Slack receivers"
   type        = list(any)
 }
+
+variable "oidc_components_client_id" {
+  description = "OIDC ClientID used to authenticate to Grafana and Prometheus (oauth2-proxy)"
+}
+
+variable "oidc_components_client_secret" {
+  description = "OIDC ClientSecret used to authenticate to Grafana and Prometheus (oauth2-proxy)"
+}
+
+variable "oidc_issuer_url" {
+  description = "Issuer URL used to authenticate to Grafana and Prometheus (oauth2-proxy)"
+}
